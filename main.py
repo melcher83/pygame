1eaefb3b356e098183434e19493e0540f9088fee
jmelcher <jonathan@bluewiretech.com>
2019-01-25 08:42:50 -0600

    Jumping

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pygame
 
 pygame.init()
 
+s_width=500
+s_height=500
 
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((s_width,s_height))
 
 pygame.display.set_caption("First Game")
 
@@ -12,9 +14,12 @@ y=50
 
 width = 40
 height = 60
-vel = 15
+vel = 5
 
 run=True
+
+isJump=False
+jumpCount =  10
 
 while run:
     pygame.time.delay(10)
@@ -23,17 +28,34 @@ while run:
             run = False
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
 
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < s_width - width - vel:
         x += vel
 
-    if keys[pygame.K_UP]:
-        y -= vel
+    if not (isJump):
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
 
-    if keys[pygame.K_DOWN]:
-        y+= vel
+        if keys[pygame.K_DOWN] and y < s_height - height - vel:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg=1
+            if jumpCount < 0:
+                neg =-1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+
+        else:
+            isJump = False
+            jumpCount=10
+
+
+
 
     win.fill((0,0,0))
     pygame.draw.rect(win,(255,0,0),(x,y,width,height))
